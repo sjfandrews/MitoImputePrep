8fbf028c765b8dbfeabe8cdb327d5ae1146c7852
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-08-06 13:08:06 +1000

    added ref panel v4 with maf of 0.5 pc

--- scripts/BASH/MassDeploy_ThousandGenomes_raijin.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_raijin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STRAND_LIST=~/GitCode/MitoImpute/scripts/INFORMATION_LISTS/b37_platforms.txt
+STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt
 
 for i in `cat ${STRAND_LIST}`; do
 	qsub -v MtPlatforms=${i} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_raijin.sh
