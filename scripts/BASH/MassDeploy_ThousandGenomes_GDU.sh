9f76ab6b9bb7b40b7b979ebe25e12a82c5dfabd2
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-08-08 10:01:23 +1000

    added GDU versions of 1,000 genomes files

---
+++ scripts/BASH/MassDeploy_ThousandGenomes_GDU.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt
+
+for i in `cat ${STRAND_LIST}`; do
+	qsub -v MtPlatforms=${i} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_GDU.sh
+	echo "JOB FOR ${i} SUBMITTED"
+done
