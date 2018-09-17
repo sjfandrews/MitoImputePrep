b775b3993cb8d29e72d57c8485f079f1da588b42
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-09-17 16:06:27 +1000

    added scripts for testing MCMC

--- scripts/BASH/MassDeploy_ThousandGenomes_raijin_MCMCtest.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_raijin_MCMCtest.sh
@@ -2,10 +2,10 @@
 
 STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt
 MCMC_list=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/MCMC_list.txt
-MCMC="1"
-BURN="0"
+MCMC="15"
+BURN="5"
 
 for i in `cat ${STRAND_LIST}`; do
-	qsub -v MtPlatforms=${i},mcmc=${MCMC} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_raijin_MCMCtest.sh
+	qsub -v MtPlatforms=${i},mcmc=${MCMC},burn=${BURN} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_raijin_MCMCtest.sh
 	echo "JOB FOR ${i} SUBMITTED"
 done
