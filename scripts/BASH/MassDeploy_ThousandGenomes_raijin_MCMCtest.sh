5df858b045af1e696fd7b658192da7e044f98504
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-09-18 15:07:25 +1000

    minor

--- scripts/BASH/MassDeploy_ThousandGenomes_raijin_MCMCtest.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_raijin_MCMCtest.sh
@@ -5,6 +5,8 @@ MCMC_list=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/MCMC_list.txt
 MCMC="20"
 BURN="6"
 
+#MCMC 1,0	5,1	10,3	20,6	30,10
+
 for i in `cat ${STRAND_LIST}`; do
 	qsub -v MtPlatforms=${i},mcmc=${MCMC},burn=${BURN} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_raijin_MCMCtest.sh
 	echo "JOB FOR ${i} SUBMITTED WITH MCMC LENGTH = ${MCMC} AND ${BURN} BURN-INS"
