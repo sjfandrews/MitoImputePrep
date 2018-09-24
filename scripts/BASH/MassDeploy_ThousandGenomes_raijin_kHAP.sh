724f9b740a80b2f88f25583455c7391c927d2123
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-09-24 12:06:01 +1000

    edited scripts to test the effect of the khap option

--- scripts/BASH/MassDeploy_ThousandGenomes_raijin_kHAP.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_raijin_kHAP.sh
@@ -4,11 +4,11 @@ STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt
 MCMC_list=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/MCMC_list.txt
 MCMC="1"
 BURN="0"
-KHAP="100"
+KHAP="250"
 
 #-k_hap 100, 250, 500, 1000, 2500, 5000, 10000, 20000, 30000
 
 for i in `cat ${STRAND_LIST}`; do
 	qsub -v MtPlatforms=${i},mcmc=${MCMC},burn=${BURN},khap=${KHAP} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_raijin_MCMCtest.sh
-	echo "JOB FOR ${i} SUBMITTED WITH MCMC LENGTH = ${MCMC} AND ${BURN} BURN-INS"
+	echo "JOB FOR ${i} SUBMITTED WITH MCMC LENGTH = ${MCMC} (${BURN} BURN-INS) AND -k_hap = ${KHAP}" 
 done
