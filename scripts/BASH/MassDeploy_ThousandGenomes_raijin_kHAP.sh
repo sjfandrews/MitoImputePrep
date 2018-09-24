a644f2f98200e3995f63077c57ef912ab87c0ae1
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-09-24 12:46:53 +1000

    edited scripts to test the effect of the khap option

--- scripts/BASH/MassDeploy_ThousandGenomes_raijin_kHAP.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_raijin_kHAP.sh
@@ -4,7 +4,7 @@ STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt
 MCMC_list=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/MCMC_list.txt
 MCMC="1"
 BURN="0"
-KHAP="100"
+KHAP="250"
 
 #-k_hap 100, 250, 500, 1000, 2500, 5000, 10000, 20000, 30000
 
