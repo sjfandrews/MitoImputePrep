184fa3b2c2dfb6dfeed52b4bed8f5f1c717e46b2
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-12-10 17:33:31 +1100

    added script to clean up concordance tables

---
+++ scripts/R/Cleanup_concordance_tables.R
@@ -0,0 +1,45 @@
+require(ggplot2)
+require(tidyr)
+
+## MAF 
+f = "~/GitCode/MitoImputePrep/metadata/ConcordanceTables_MAF_Combined.csv"
+x = read.csv(f, header = T)
+x[,2] = factor(x[,2])
+x2 = x
+
+x = x[-(3:9)]
+x = spread(x, names(x)[2], "Imputed.hg.Conc")
+v = names(x)[2:ncol(x)]
+#x$largest = NA
+largest = c()
+for (i in 1:nrow(x)) {
+  if (apply(x[i,2:ncol(x)], 1, function(u) all(u %in% NA))) {
+    #x$largest[i] = NA
+    largest = c(largest, NA)
+  } else {
+    #x$largest[i] = v[which(unlist(x[i,2:ncol(x)]) == max(unlist(x[i,2:ncol(x)])))]
+    largest = c(largest, paste(v[which(unlist(x[i,2:ncol(x)]) == max(unlist(x[i,2:ncol(x)]), na.rm = T))], collapse = ","))
+  }
+}
+x$largest = largest
+table(x$largest)
+
+
+x2$diff = x2$Imputed.hg.Conc - x2$Typed.hg.Conc
+x2 = x2[-(3:10)]
+x2 = spread(x2, names(x2)[2], "diff")
+#x$largest = NA
+largest = c()
+for (i in 1:nrow(x2)) {
+  if (apply(x2[i,2:ncol(x2)], 1, function(u) all(u %in% NA))) {
+    #x$largest[i] = NA
+    largest = c(largest, NA)
+  } else {
+    #x$largest[i] = v[which(unlist(x[i,2:ncol(x)]) == max(unlist(x[i,2:ncol(x)])))]
+    largest = c(largest, paste(v[which(unlist(x2[i,2:ncol(x2)]) == max(unlist(x2[i,2:ncol(x2)]), na.rm = T))], collapse = ","))
+  }
+}
+x2$largest = largest
+table(x2$largest)
+
+boxplot(x)
