78b0f089aba8e454afdceecf35c0ca24a7698412
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-12-12 09:30:44 +1100

    added excel sheet for conc tables

--- scripts/R/check_haplogroup_concordance.R
+++ scripts/R/check_haplogroup_concordance.R
@@ -11,10 +11,19 @@ full_1kGP = generate_snp_data("/Volumes/TimMcInerney/MitoImpute/data/PLINK/ALL.c
                               "/Volumes/TimMcInerney/MitoImpute/data/PLINK/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.ped")
 full_1kGP_hg_FULL = HiMC::getClassifications(full_1kGP)
 
-for (i in 1:1) { # length(chips$array)
+hg.df = data.frame(cbind(full_1kGP_hg_FULL$Individual, full_1kGP_hg_FULL$haplogroup))
+names(hg.df) = c("Individual", "WGS")
+hg.df$chip = NA
+hg.df$typed = NA
+hg.df$imputed = NA
+hg.df$match.t = NA
+hg.df$match.i = NA
+
+for (i in 1:length(chips$array)) { # length(chips$array)
   print(paste0(i, " / ", length(chips$array)))
   DIR = "/Volumes/TimMcInerney/MitoImpute/data/STRANDS/"
   mcmc = "1"
+  hg.df$chip = chips$array[i]
   
   # REF PANEL v2 (MAF >= 1%)
   if (file.exists(paste0(DIR, chips$array[i], "/MCMC_Experiments/MCMC1/chrMT_1kg_", chips$array[i], "_imputed_MCMC", mcmc,"_info"))) {
@@ -67,5 +76,14 @@ for (i in 1:1) { # length(chips$array)
     conc_imputed = full_1kGP_hg$haplogroup == imputed_1kGP_hg$haplogroup
     conc_typed_pc = length(conc_typed[conc_typed == T]) / length(conc_typed)
     conc_imputed_pc = length(conc_imputed[conc_imputed == T]) / length(conc_imputed)
+    
+    ## MERGE TABLES
+    for (j in 1:nrow(hg.df)) {
+      hg.df$typed[j] = typed_1kGP_hg$haplogroup[match(hg.df$Individual[j], typed_1kGP_hg$Individual)]
+      hg.df$imputed[j] = typed_1kGP_hg$haplogroup[match(hg.df$Individual[j], imputed_1kGP_hg$Individual)]
+      hg.df$match.t[j] = hg.df$WGS[j] == hg.df$typed[j]
+      hg.df$match.i[j] = hg.df$WGS[j] == hg.df$imputed[j]
+    }
+    write.csv(hg.df, paste0("~/GitCode/MitoImputePrep/metadata/haplogroup_concordance/", chips$array[i], "_haplogroupings.csv"))
   }
 }
