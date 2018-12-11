2fe736852e621961487bd112fbdfe0afa50e6b4b
Tim McInerney <tim.mcinerney@anu.edu.au>
2018-12-11 17:02:59 +1100

    added excel sheet for conc tables

---
+++ scripts/R/check_haplogroup_concordance.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+library(readxl)
+library(HiMC); data(nodes)
+
+## READ IN THE LIST OF CHIPS
+chips = read.table("~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt")
+names(chips) = c("array")
+
+## READ IN .ped AND .map FILES FOR TRUTH SET 
+full_1kGP = generate_snp_data("/Volumes/TimMcInerney/MitoImpute/data/PLINK/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.map",
+                              "/Volumes/TimMcInerney/MitoImpute/data/PLINK/ALL.chrMT.phase3_callmom-v0_4.20130502.genotypes.ped")
+full_1kGP_hg_FULL = HiMC::getClassifications(full_1kGP)
+
+for (i in 1:1) { # length(chips$array)
+  print(paste0(i, " / ", length(chips$array)))
+  DIR = "/Volumes/TimMcInerney/MitoImpute/data/STRANDS/"
+  mcmc = "1"
+  
+  # REF PANEL v2 (MAF >= 1%)
+  if (file.exists(paste0(DIR, chips$array[i], "/MCMC_Experiments/MCMC1/chrMT_1kg_", chips$array[i], "_imputed_MCMC", mcmc,"_info"))) {
+    ## READ IN THE INFO SCORE
+    info.score <- read_delim(paste0(DIR, chips$array[i], "/MCMC_Experiments/MCMC1/chrMT_1kg_", chips$array[i], "_imputed_MCMC", mcmc,"_info"), delim = " ")
+    mean.info = mean(info.score$info)
+    exclude.info.score = subset(info.score, !(info.score$info >= info.cutoff))
+    exclude.pos = as.character(exclude.info.score$position)
+    
+    ## FILTER VARIANTS IN TRUTH SET
+    full_1kGP_cut = full_1kGP[, -(which(names(full_1kGP) %in% exclude.pos))]
+    full_1kGP_hg = HiMC::getClassifications(full_1kGP_cut)
+    full_index_exclude = which(names(full_1kGP) %in% exclude.pos)
+    if (length(full_index_exclude) > 0) {
+      full_1kGP_cut = full_1kGP[, -full_index_exclude]
+      full_1kGP_hg = HiMC::getClassifications(full_1kGP_cut)
+      message("TRUTH HAPLOTYPES CLASSIFIED")
+    } else {
+      message("CAUTION! NOT FILTERING")
+      message("PULLING FROM FULL LIST")
+      full_1kGP_hg = full_1kGP_hg_FULL
+    }
+    
+    ## READ IN .ped AND .map FILES FOR IMPUTED SET 
+    message(paste("READING IN .ped AND .map FILES FOR ", chips$array[i], " MCMC = ", mcmc))
+    imputed_1kGP <- generate_snp_data(paste0(DIR, chips$array[i], "/MCMC_Experiments/MCMC1/chrMT_1kg_", chips$array[i], "_imputed_MCMC", mcmc, ".map"),
+                                      paste0(DIR, chips$array[i], "/MCMC_Experiments/MCMC1/chrMT_1kg_", chips$array[i], "_imputed_MCMC", mcmc, ".ped"))
+    imputed_index_exclude = which(names(imputed_1kGP) %in% exclude.pos)
+    if (length(imputed_index_exclude) > 0) {
+      imputed_1kGP_cut = imputed_1kGP[, -imputed_index_exclude]
+      imputed_1kGP_hg = HiMC::getClassifications(imputed_1kGP_cut)
+    } else {
+      imputed_1kGP_hg = HiMC::getClassifications(imputed_1kGP)
+    }
+    message("IMPUTED HAPLOTYPES CLASSIFIED")
+    
+    typed_1kGP <- generate_snp_data(paste0(DIR, chips$array[i], "/ReferencePanel_v2/chrMT_1kg_", chips$array[i], ".map"),
+                                    paste0(DIR, chips$array[i], "/ReferencePanel_v2/chrMT_1kg_", chips$array[i], ".ped"))
+    typed_index_exclude = which(names(typed_1kGP) %in% exclude.pos)
+    if (length(typed_index_exclude) > 0) {
+      typed_1kGP_cut = typed_1kGP[, -typed_index_exclude]
+      typed_1kGP_hg = HiMC::getClassifications(typed_1kGP_cut)
+    } else {
+      typed_1kGP_hg = HiMC::getClassifications(typed_1kGP)
+    }
+    message("TYPED HAPLOTYPES CLASSIFIED")
+    
+    ## CALCULATE HAPLOGROUP CONCORDANCE
+    conc_typed = full_1kGP_hg$haplogroup == typed_1kGP_hg$haplogroup
+    conc_imputed = full_1kGP_hg$haplogroup == imputed_1kGP_hg$haplogroup
+    conc_typed_pc = length(conc_typed[conc_typed == T]) / length(conc_typed)
+    conc_imputed_pc = length(conc_imputed[conc_imputed == T]) / length(conc_imputed)
+  }
+}
