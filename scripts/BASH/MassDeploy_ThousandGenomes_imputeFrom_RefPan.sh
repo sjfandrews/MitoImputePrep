bccc8d3cd99194c62e2a76a66f9e5675d29c3a4d
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-02-03 23:24:08 +1100

    minor

--- scripts/BASH/MassDeploy_ThousandGenomes_imputeFrom_RefPan.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_imputeFrom_RefPan.sh
@@ -10,6 +10,6 @@ REFpanel=$1
 #MCMC 1,0	5,1	10,3	20,6	30,10
 
 for i in `cat ${STRAND_LIST}`; do
-	qsub -v REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_imputeFrom_RefPan_v6.sh
+	qsub -v REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_imputeFrom_RefPan.sh
 	echo "JOB FOR ${i} SUBMITTED FOR ${REFpanel} WITH MCMC LENGTH = ${MCMC} AND ${BURN} BURN-INS"
 done
