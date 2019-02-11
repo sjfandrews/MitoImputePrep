5797f3ccb58cc9f672812b284bda59562661250a
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-02-11 12:05:27 +1100

    minor

--- scripts/BASH/MassDeploy_ThousandGenomes_imputeFrom_RefPan_testKHAP.sh
+++ scripts/BASH/MassDeploy_ThousandGenomes_imputeFrom_RefPan_testKHAP.sh
@@ -6,7 +6,7 @@ MCMC_list=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/MCMC_list.txt
 REFpanel=$1
 MCMC="1"
 BURN="0"
-#KHAP=$2
+KHAP=$2
 NE="20000"
 
 echo
@@ -18,10 +18,15 @@ echo "sh ~/GitCode/MitoImputePrep/scripts/BASH/MassDeploy_ThousandGenomes_impute
 
 declare -a arr=("100" "250" "500" "1000" "2500" "5000" "10000" "20000" "30000")
 
-for KHAP in "${arr[@]}"
-do
-   for i in `cat ${STRAND_LIST}`; do
-		qsub -v REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN},khap=${KHAP},ne=${NE} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_imputeFrom_RefPan_testKHAP.sh
-		echo "JOB FOR ${i} SUBMITTED FOR ${REFpanel} WITH PARAMETERS:	MCMC LENGTH = ${MCMC} (${BURN} BURN-INS),	K_HAP = ${KHAP},	NE = ${NE}"
-	done
+#for KHAP in "${arr[@]}"
+#do
+#   for i in `cat ${STRAND_LIST}`; do
+#		qsub -v REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN},khap=${KHAP},ne=${NE} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_imputeFrom_RefPan_testKHAP.sh
+#		echo "JOB FOR ${i} SUBMITTED FOR ${REFpanel} WITH PARAMETERS:	MCMC LENGTH = ${MCMC} (${BURN} BURN-INS),	K_HAP = ${KHAP},	NE = ${NE}"
+#	done
+#done
+
+for i in `cat ${STRAND_LIST}`; do
+	qsub -v REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN},khap=${KHAP},ne=${NE} ~/GitCode/MitoImputePrep/scripts/BASH/ThousandGenomes_imputeFrom_RefPan_testKHAP.sh
+	echo "JOB FOR ${i} SUBMITTED FOR ${REFpanel} WITH PARAMETERS:	MCMC LENGTH = ${MCMC} (${BURN} BURN-INS),	K_HAP = ${KHAP},	NE = ${NE}"
 done
