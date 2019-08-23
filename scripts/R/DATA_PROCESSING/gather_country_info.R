7d5d90e4189d66c8f88088a1ffd63964a229d6ff
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-08-23 14:46:23 +1000

    minor

---
+++ scripts/R/DATA_PROCESSING/gather_country_info.R
@@ -0,0 +1,36 @@
+x = entrez_search("nuccore", term="EF184582.1")
+y = entrez_fetch("nuccore", id = x$ids, rettype = "native")
+z = str_split(y, "\n")
+z = lapply(z, str_trim)
+z = unlist(z)
+
+m = which("subtype country,"==z)
+z[m[1]+1] %>%
+  str_remove(., 'name \"') %>%
+  str_remove(., '\"')
+
+countries = c()
+seq_list = read.table("~/Desktop/seq_country_test.txt", header = F)
+names(seq_list) = c("seqID")
+seq_list$Country = NA
+
+for (i in 1:nrow(seq_list)) {
+  if (i %% 1000 == 0) {
+    message(paste0(i , "  /  ", nrow(seq_list))) 
+  }
+  tmp_seq = as.character(seq_list$seqID[i])
+  x = entrez_search("nuccore", term = tmp_seq)
+  y = entrez_fetch("nuccore", id = x$ids, rettype = "native")
+  z = str_split(y, "\n")
+  z = lapply(z, str_trim)
+  z = unlist(z)
+  
+  m = which("subtype country,"==z)
+  tmp_country = z[m[1]+1] %>%
+    str_remove(., 'name \"') %>%
+    str_remove(., '\"')
+  
+  seq_list$Country[i] = tmp_country
+}
+
+write.csv(seq_list, "~/GitCode/MitoImputePrep/metadata/seq_country_list.csv", row.names = F, quote = F)
