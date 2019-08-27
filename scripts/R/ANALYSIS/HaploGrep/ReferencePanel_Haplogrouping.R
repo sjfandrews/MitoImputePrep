bb8f2783fe011ce896fbb82cf69527b8418b8609
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-08-27 15:40:46 +1000

    minor

---
+++ scripts/R/ANALYSIS/HaploGrep/ReferencePanel_Haplogrouping.R
@@ -0,0 +1,3 @@
+haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.csv"
+
+haplogroups = read.csv(haplogrep_file, header = T)
