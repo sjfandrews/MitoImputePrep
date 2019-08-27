0e02898a7f2692a67e616293119433d4bea16090
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-08-27 15:52:37 +1000

    minor

--- scripts/R/ANALYSIS/HaploGrep/ReferencePanel_Haplogrouping.R
+++ scripts/R/ANALYSIS/HaploGrep/ReferencePanel_Haplogrouping.R
@@ -1,3 +1,16 @@
+library(dplyr)
+
 haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.csv"
 
 haplogroups = read.csv(haplogrep_file, header = T)
+
+nonAFR = subset(haplogroups, substr(haplogroups$Haplogroup, 1, 1) != "L")
+nonAFR$Macrohaplogroup = substr(nonAFR$Haplogroup, 1, 1)
+
+AFR = subset(haplogroups, substr(haplogroups$Haplogroup, 1, 1) == "L")
+AFR$Macrohaplogroup = substr(AFR$Haplogroup, 1, 2)
+
+fixed_haplogroups = rbind(nonAFR, AFR)
+fixed_haplogroups = arrange(fixed_haplogroups, fixed_haplogroups$SampleID)
+
+write.csv(fixed_haplogroups, haplogrep_file, quote = F, row.names = F)
