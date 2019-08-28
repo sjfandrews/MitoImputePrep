e4d0ae33ff6ae273f1e5366237fd1d46b63b7bd4
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-08-28 14:45:17 +1000

    minor

--- scripts/R/ANALYSIS/HaploGrep/EastealAlignment_Haplogrouping.R
+++ scripts/R/ANALYSIS/HaploGrep/EastealAlignment_Haplogrouping.R
@@ -1,8 +1,8 @@
 library(dplyr)
 
-haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.txt"
-haplogrep_csv = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.csv"
-haplogroup_table_csv = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar_freTable.csv"
+haplogrep_file = "/Volumes/TimMcInerney/Master_Alignment/VCF/hsapiensCRS7k_ambigANDgap2missing_HaploGrep.txt"
+haplogrep_csv = "/Volumes/TimMcInerney/Master_Alignment/VCF/hsapiensCRS7k_ambigANDgap2missing_HaploGrep.csv"
+haplogroup_table_csv = "/Volumes/TimMcInerney/Master_Alignment/VCF/hsapiensCRS7k_ambigANDgap2missing_HaploGrep_freTable.csv"
 
 haplogroups = read.table(haplogrep_file, header = T, sep = "\t")
 
@@ -17,37 +17,8 @@ fixed_haplogroups = arrange(fixed_haplogroups, fixed_haplogroups$SampleID)
 
 write.csv(fixed_haplogroups, haplogrep_csv, quote = F, row.names = F)
 
-full_haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_noInvar_HaploGrep.txt"
-full_haplogrep_csv = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_noInvar_HaploGrep.csv"
-haplogroup_table_csv = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_noInvar_HaploGrep_freqTable.csv"
-
-full_haplogroups = read.table(full_haplogrep_file, header = T, sep = "\t")
-full_nonAFR = subset(full_haplogroups, substr(full_haplogroups$Haplogroup, 1, 1) != "L")
-full_nonAFR$Macrohaplogroup = substr(full_nonAFR$Haplogroup, 1, 1)
-
-full_AFR = subset(full_haplogroups, substr(full_haplogroups$Haplogroup, 1, 1) == "L")
-full_AFR$Macrohaplogroup = substr(full_AFR$Haplogroup, 1, 2)
-
-full_fixed_haplogroups = rbind(full_nonAFR, full_AFR)
-full_fixed_haplogroups = arrange(full_fixed_haplogroups, full_fixed_haplogroups$SampleID)
-
-write.csv(full_fixed_haplogroups, full_haplogrep_csv, quote = F, row.names = F)
-
-# FILTERED HAPLOGROUPS TABLE
-table(fixed_haplogroups$Macrohaplogroup)
-
-# FULL HAPLOGROUPS TABLE
-table(full_fixed_haplogroups$Macrohaplogroup)
-
-# DIFFERENCE HAPLOGROUP TABLE
-(table(full_fixed_haplogroups$Macrohaplogroup) - table(fixed_haplogroups$Macrohaplogroup)) / sum(table(full_fixed_haplogroups$Macrohaplogroup) - table(fixed_haplogroups$Macrohaplogroup)) * 100
-nrow(full_fixed_haplogroups) - nrow(fixed_haplogroups)
-
 freqTable = as.data.frame(table(fixed_haplogroups$Macrohaplogroup))
-freqTable$Freq2 = as.data.frame(table(full_fixed_haplogroups$Macrohaplogroup))[,2]
-freqTable$Diff = as.data.frame((table(full_fixed_haplogroups$Macrohaplogroup) - table(fixed_haplogroups$Macrohaplogroup)))[,2]
-freqTable$DiffPC = as.data.frame((table(full_fixed_haplogroups$Macrohaplogroup) - table(fixed_haplogroups$Macrohaplogroup)) / sum(table(full_fixed_haplogroups$Macrohaplogroup) - table(fixed_haplogroups$Macrohaplogroup)))[,2]
-names(freqTable) = c("Haplogroup", "FullRefPanel", "FiltRefPanel", "Diff", "DiffPC")
+names(freqTable) = c("Haplogroup", "Freq")
 freqTable
 
 write.csv(freqTable, haplogroup_table_csv, quote = F, row.names = F)
