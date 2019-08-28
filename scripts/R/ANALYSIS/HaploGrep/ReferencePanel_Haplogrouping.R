6f0c60f99bfa20167c8b38dc4afb38221022a3d3
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-08-28 10:03:31 +1000

    minor

--- scripts/R/ANALYSIS/HaploGrep/ReferencePanel_Haplogrouping.R
+++ scripts/R/ANALYSIS/HaploGrep/ReferencePanel_Haplogrouping.R
@@ -1,8 +1,8 @@
 library(dplyr)
 
-haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.csv"
+haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.txt"
 
-haplogroups = read.csv(haplogrep_file, header = T)
+haplogroups = read.table(haplogrep_file, header = T)
 
 nonAFR = subset(haplogroups, substr(haplogroups$Haplogroup, 1, 1) != "L")
 nonAFR$Macrohaplogroup = substr(nonAFR$Haplogroup, 1, 1)
@@ -14,3 +14,5 @@ fixed_haplogroups = rbind(nonAFR, AFR)
 fixed_haplogroups = arrange(fixed_haplogroups, fixed_haplogroups$SampleID)
 
 write.csv(fixed_haplogroups, haplogrep_file, quote = F, row.names = F)
+
+full_haplogrep_file = "/Volumes/TimMcInerney/MitoImpute/data/VCF/McInerney_Master_Alignment_July18_2018_ambigANDgap2missing_tagFilled_filt_noInvar.txt"
