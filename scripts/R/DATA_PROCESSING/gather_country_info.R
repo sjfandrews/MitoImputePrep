61acf3d1fc85f1e00db243cd4cc9e38ee892f45e
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-08-29 08:42:35 +1000

    minor

--- scripts/R/DATA_PROCESSING/gather_country_info.R
+++ scripts/R/DATA_PROCESSING/gather_country_info.R
@@ -24,7 +24,7 @@ if (file.exists(outFile) == T) {
   seq_list = read.csv(outFile, header = T)
 }
 
-for (i in 1:nrow(seq_list)) {
+for (i in 1:nrow(seq_list)) { #nrow(seq_list)
   if (i %% 100 == 0) {
     message(paste0(i , "  /  ", nrow(seq_list))) 
   }
@@ -33,25 +33,27 @@ for (i in 1:nrow(seq_list)) {
     
     tmp_seq = as.character(seq_list$seqID[i])
     x = entrez_search("nuccore", term = tmp_seq)
-    y = entrez_fetch("nuccore", id = x$ids, rettype = "native")
-    z = str_split(y, "\n")
-    z = lapply(z, str_trim)
-    z = unlist(z)
-    
-    m = which("subtype country,"==z)
-    tmp_country = z[m[1]+1] %>%
-      str_remove(., 'name \"') %>%
-      str_remove(., '\"') %>%
-      str_replace(., pattern = ",", replacement = ";")
-    
-    seq_list$Country[i] = tmp_country
+    if (length(x$ids) > 0) {
+      y = entrez_fetch("nuccore", id = x$ids, rettype = "native")
+      z = str_split(y, "\n")
+      z = lapply(z, str_trim)
+      z = unlist(z)
+      
+      m = which("subtype country,"==z)
+      tmp_country = z[m[1]+1] %>%
+        str_remove(., 'name \"') %>%
+        str_remove(., '\"') %>%
+        str_replace(., pattern = ",", replacement = ";")
+      
+      seq_list$Country[i] = tmp_country
+    }
     seq_list$Checked[i] = T
   }
   
   if (i %% 100 == 0) {
     write.csv(seq_list, outFile, row.names = F, quote = F)
   }
-
+  
 }
 
 message(paste0("LAST i  =  ", i))
