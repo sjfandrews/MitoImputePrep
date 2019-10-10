d04ffb764b48ec9f7c5866e1b117588839e46368
Tim McInerney <tim.mcinerney@anu.edu.au>
2019-10-10 14:45:14 +1100

    minor

--- scripts/R/DATA_PROCESSING/create_DLOOP_chip.R
+++ scripts/R/DATA_PROCESSING/create_DLOOP_chip.R
@@ -6,7 +6,7 @@ rep("MT", length(c(HVRI, HVRII)))
 
 df = data.frame(CHR = rep("MT", length(c(HVRI, HVRII))), SITE = c(HVRI, HVRII))
 
-out_file = " /g/data1a/te53/MitoImpute/data/STRANDS/DLOOP_1-577_16023-16569/DLOOP_1-577_16023-16569_MT_snps.txt"
+out_file = "/g/data1a/te53/MitoImpute/data/STRANDS/DLOOP_1-577_16023-16569/DLOOP_1-577_16023-16569_MT_snps.txt"
 
 write.table(x = df,
             file = out_file,
