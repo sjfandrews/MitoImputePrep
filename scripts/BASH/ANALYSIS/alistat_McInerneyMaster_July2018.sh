292ef27c6d77c8e180f520654e5b3470017743dc
Tim McInerney <tim.mcinerney@anu.edu.au>
2020-02-04 14:10:40 +1100

    minor

---
+++ scripts/BASH/ANALYSIS/alistat_McInerneyMaster_July2018.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -P gw26
+#PBS -q biodev
+#PBS -l walltime=48:00:00
+#PBS -l mem=24GB
+#PBS -l ncpus=1
+#PBS -m e
+#PBS -M u5015730@anu.edu.au
+#PBS -j oe
+#PBS -o /g/data1a/te53/haploco/logs/
+#PBS -l storage=scratch/te53+gdata/te53
+#PBS -N AliStat_McInerneyMaster_July2018
+
+
+# LOAD THE MODULE FILE
+module load AliStat
+
+# SET VARIABLES
+FASTA=/g/data1a/te53/MitoImpute/data/FASTA/masters/McInerney_Master_Alignment_July18_2018.fasta
+OUT_DIR=/g/data1a/te53/MitoImpute/AliStat/test_aln/
+OUT_FILE=${OUT_DIR}`basename ${FASTA} .fasta`
+
+alistat ${FASTA} 1 -o ${OUT_FILE} -i -d
+# END !
---
+++ scripts/R/DATA_PROCESSING/reanalyse_country_labels.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+
+f = "~/GitCode/MitoImputePrep/metadata/seq_country_list.csv"
+
+seq_list = read_csv(f, col_names = T)
+
+seq_list$Region = gsub(".*:", "", seq_list$Country)
+seq_list$Region = str_trim(seq_list$Region)
+seq_list$Region[seq_list$Country == seq_list$Region] = NA
+seq_list$Country_short = gsub("\\:.*","", seq_list$Country)
+seq_list$Continent = countrycode(sourcevar = seq_list$Country_short, origin = "country.name", destination = "continent")
+
+region_table    = as.data.frame(table(seq_list$Country))
+country_table   = as.data.frame(table(seq_list$Country_short))
+continent_table = as.data.frame(table(seq_list$Continent))
+
+as_tibble(region_table)
+as_tibble(country_table)
+as_tibble(continent_table)
+
+
+table(is.na(seq_list$Region))
