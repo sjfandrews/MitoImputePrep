a9aafdab7495b83ce1eaf94c75b94029e3b2db64
Tim McInerney <u5015730@anu.edu.au>
2020-07-17 17:17:01 +1000

    minor

--- scripts/BASH/DATA_PROCESSING/ThousandGenomes_Imputation/MassDeploy_ThousandGenomes_imputeFrom_RefPan.sh
+++ scripts/BASH/DATA_PROCESSING/ThousandGenomes_Imputation/MassDeploy_ThousandGenomes_imputeFrom_RefPan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt
-STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms_smallTest2.txt
+STRAND_LIST=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms_smallTest.txt
 MCMC_list=~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/MCMC_list.txt
 
 REFpanel=$1
@@ -18,13 +18,11 @@ echo "sh ~/GitCode/MitoImputePrep/scripts/BASH/MassDeploy_ThousandGenomes_impute
 
 for i in `cat ${STRAND_LIST}`; do
 	summary_file=/g/data1a/te53/MitoImpute/data/STRANDS/${i}/${REFpanel}/MCMC${MCMC}/chrMT_1kg_${i}_imputed_MCMC${MCMC}_SUMMARY.csv
-	echo ${summary_file}
+	#echo ${summary_file}
 	
 	if [ ! -s ${summary_file} ]
 	then
-		echo qsub -v nqstat_anu=nqstat_anu,REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN},khap=${KHAP},ne=${NE} -N IMPUTE2_${i}_${REFpanel}_MCMC${MCMC}_kHAP${KHAP} ~/GitCode/MitoImputePrep/scripts/BASH/DATA_PROCESSING/ThousandGenomes_Imputation/ThousandGenomes_imputeFrom_RefPan.sh
+		qsub -v nqstat_anu=nqstat_anu,REFpanel=${REFpanel},MtPlatforms=${i},mcmc=${MCMC},burn=${BURN},khap=${KHAP},ne=${NE} -N IMPUTE2_${i}_${REFpanel}_MCMC${MCMC}_kHAP${KHAP} ~/GitCode/MitoImputePrep/scripts/BASH/DATA_PROCESSING/ThousandGenomes_Imputation/ThousandGenomes_imputeFrom_RefPan.sh
 		echo "JOB FOR ${i} SUBMITTED FOR ${REFpanel} WITH PARAMETERS:	MCMC LENGTH = ${MCMC} (${BURN} BURN-INS),	K_HAP = ${KHAP},	NE = ${NE}"
-	else
-		echo "${summary_file} FOUND	...	PASSING"
 	fi
 done
