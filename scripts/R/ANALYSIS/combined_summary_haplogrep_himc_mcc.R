4a31d0293d2bf89b632c60deb71554fa531b3e24
Tim McInerney <u5015730@anu.edu.au>
2020-07-20 19:53:26 +1000

    minor

--- scripts/R/ANALYSIS/combined_summary_haplogrep_himc_mcc.R
+++ scripts/R/ANALYSIS/combined_summary_haplogrep_himc_mcc.R
@@ -3,6 +3,7 @@ library(tidyverse)
 wd = "/g/data1a/te53/MitoImpute/data/STRANDS/"
 
 out_file1 = "/g/data1a/te53/MitoImpute/analyses/combined_summaries/MAF_combined.csv"
+out_file2 = "/g/data1a/te53/MitoImpute/analyses/combined_summaries/kHAP_combined.csv"
 
 strands = read_tsv("~/GitCode/MitoImputePrep/scripts/INFORMATION_LISTS/b37_platforms.txt", col_names = F)
 
@@ -13,6 +14,8 @@ khap.var = c("kHAP100", "kHAP250", "kHAP500", "kHAP1000", "kHAP2500", "kHAP5000"
 maf.dir = "MAF_Experiments"
 maf.panel = c("ReferencePanel_v2", "ReferencePanel_v4", "ReferencePanel_v3")
 maf.var = c("MAF1%", "MAF0.5%", "MAF0.1%")
+
+# MAF EXPERIMENTS
 
 for (i in 1:length(maf.panel)) {
   # SET REFERENCE PANEL
@@ -63,3 +66,53 @@ if (file.exists(out_file1)) {
 }
 
 print(combined_summary)
+
+# KHAP EXPERIMENTS
+
+for (i in 1:length(khap.var)) {
+  # SET REFERENCE PANEL
+  khap =  khap.var[i]
+  
+  for (j in 1:nrow(strands)) {
+    # SET STRAND
+    strand = strands$X1[j]
+    
+    # ECHO INFO
+    cat("WORKING ON ", khap, " | ", strand, "\r")
+    
+    # SET SUMMARY FILE
+    summary_file = paste0(wd, strand, "/kHAP_Experiments/", khap, "/chrMT_1kg_", strand, "_imputed_", khap, "_SUMMARY.csv")
+    
+    if (i == 1 && j == 1) {
+      # READ IN SUMMARY FILE AS COMBINED SUMMARY IF FIRST IN LIST
+      combined_summary = read_csv(summary_file, col_names = T)
+    } else {
+      # OTHERWISE CHECK TO SEE IF FILE EXISTS
+      if (file.exists(summary_file)) {
+        # IF IT FOES, READ IT IN, THEN COMBINED
+        tmp_summary = read_csv(summary_file, col_names = T)
+        combined_summary = bind_rows(combined_summary, tmp_summary)
+      } else {
+        # IF NOT, CREATE AN EMPTY DATA FRAME WITH THE ARRAY NAME AND IMPUTED AS FALSE
+        tmp_empty_summary = as_tibble(data.frame(matrix(NA, nrow = 1, ncol = ncol(combined_summary))))
+        names(tmp_empty_summary) = names(combined_summary)
+        tmp_empty_summary$array[1] = strand
+        tmp_empty_summary$imputed[1] = FALSE
+        tmp_empty_summary$refpan_maf = "MAF1%"
+        tmp_empty_summary$mcmc = "MCMC1"
+        tmp_empty_summary$k_hap = khap
+        combined_summary = bind_rows(combined_summary, tmp_empty_summary)
+      }
+      
+    }
+    
+  }
+  
+}
+
+
+write_csv(x = combined_summary, path = out_file2)
+
+if (file.exists(out_file1)) {
+  message(paste0("OUTPUT FILE WRITTEN TO:  ", out_file2))
+}
